1ac0e8add07a120ddf105e1f250592dbd9b75082
Rashi Rajaura <rashirajaura0603@gmail.com>
2026-07-29 22:35:44 +0530

    33th Commit

---
+++ Day33.c
@@ -0,0 +1,68 @@
+/* Problem: Convert an infix expression to postfix notation using stack.
+
+Input:
+- Single line: infix expression (operands are single characters)
+
+Output:
+- Print the postfix expression
+
+Example:
+Input:
+A+B*C
+
+Output:
+ABC*+
+
+Explanation:
+Operator precedence: * > +
+Use stack to handle operator precedence and associativity */
+#include <stdio.h>
+#include <ctype.h>
+#define MAX 1000
+char stack[MAX];
+int top = -1;
+void push(char c) {
+    stack[++top] = c;
+}
+char pop() {
+    return stack[top--];
+}
+char peek() {
+    return stack[top];
+}
+int precedence(char op) {
+    if (op == '^') return 3;
+    if (op == '*' || op == '/') return 2;
+    if (op == '+' || op == '-') return 1;
+    return 0;
+}
+int main() {
+    char infix[MAX];
+    scanf("%s", infix);
+    for (int i = 0; infix[i] != '\0'; i++) {
+        char ch = infix[i];
+        if (isalnum(ch)) {
+            printf("%c", ch);
+        }
+        else if (ch == '(') {
+            push(ch);
+        }
+        else if (ch == ')') {
+            while (top != -1 && peek() != '(')
+                printf("%c", pop());
+            if (top != -1)
+                pop();
+        }
+        else {
+            while (top != -1 && peek() != '(' &&
+                   (precedence(peek()) > precedence(ch) ||
+                   (precedence(peek()) == precedence(ch) && ch != '^'))) {
+                printf("%c", pop());
+            }
+            push(ch);
+        }
+    }
+    while (top != -1)
+        printf("%c", pop());
+    return 0;
+}
